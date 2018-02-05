c1bb1fa2559963cde65a25adc309ee8b7c767a54
Ougui9 <33538250+Ougui9@users.noreply.github.com>
2018-02-05 17:34:44 -0500

    finished the data importing and process

--- helper.py
+++ helper.py
@@ -5,38 +5,94 @@ import numpy as np
 
 # def deg2rad(deg):
 #     return deg/180*np.pi
+#
+# def impData():
+#     sensitivity_A = 0.33  # unit:mv/g
+#     scale_A = 3.3 / 1023 / sensitivity_A
+#     Az_addbias =1/scale_A
+#
+#     sensitivity_W = 3.33   # unit:mv/deg/s
+#     scale_W = 3300 / 1023 / (sensitivity_W*180/np.pi)
+#
+#
+#     data = io.loadmat('./imu/imuRaw1.mat')
+#     raw_vals, ts = data['vals'], data['ts']
+#     A,W=raw_vals[:3,:],raw_vals[3:,:]
+#     A_bias=np.mean(A[:,:100],axis=1)
+#     A_bias[-1]=A_bias+Az_addbias
+#     A=(A-A_bias)*scale_A
+#
+#     W_bias = np.mean(W[:, :100], axis=1)
+#     W = (W - W_bias) * scale_W
+#
+#
+#
+#     return A,W
+#
+#
+# def gtData():
+#
+#
+#     data=io.loadmat('./vicon/viconRot1.mat')
+#     rots, ts_gt= data['rots'], data['ts']
+#
+#
+#     return rots,ts_gt
 
-def impData():
-    sensitivity_A = 0.33  # unit:mv/g
-    scale_A = 3.3 / 1023 / sensitivity_A
-    Az_addbias =1/scale_A
+def acc2rp(acc):
+    # r=-np.arctan(acc[1]/np.sqrt(acc[0]**2+acc[2]**2))
+    r = -np.arctan(acc[1] / acc[2])
+    p=np.arctan(acc[0]/np.sqrt(acc[1]**2+acc[2]**2))
+    y=np.zeros_like(r)
+    return r,p,y
 
-    sensitivity_W = 3.33   # unit:mv/deg/s
-    scale_W = 3300 / 1023 / (sensitivity_W*180/np.pi)
+def vec2quat(vec):#n*3--->n*4
+    theta =np.linalg.norm(vec)
+    vec_unit=vec/theta
+    q=np.zeros(vec.shape[1]+1)
+    q[0],q[1:]=np.cos(theta/2),vec_unit*np.sin(theta/2)
+    return q
 
-
-    data = io.loadmat('./imu/imuRaw1.mat')
-    raw_vals, ts = data['vals'], data['ts']
-    A,W=raw_vals[:3,:],raw_vals[3:,:]
-    A_bias=np.mean(A[:,:100],axis=1)
-    A_bias[-1]=A_bias+Az_addbias
-    A=(A-A_bias)*scale_A
-
-    W_bias = np.mean(W[:, :100], axis=1)
-    W = (W - W_bias) * scale_W
-
-
-
-    return A,W
-
-
-def gtData():
+def quatMulti(a,b):
+    a0, a1, a2, a3 = a
+    b0, b1, b2, b3 = b
+    q=np.zeros([1,4])
+    q[0],q[1],q[2],q[3]=-b1*a1 - b2*a2 - b3*a3 + b0*a0,\
+                        b1*a0 + b2*a3 - b3*a2 + b0*a1,\
+                        -b1*a3 + b2*a0 + b3*a1 + b0*a2,\
+                        b1*a2 - b2*a1 + b3*a0 + b0*a3
+    return q
 
 
-    data=io.loadmat('./vicon/viconRot1.mat')
-    rots, ts_gt= data['rots'], data['ts']
+def rpy2rot(r,p,y):
+    rot=np.zeros([3,3,len(r)])
+    a = y
+    b = p
+    c = r
+    rot[0,0]=np.multiply(np.cos(a),np.cos(b))
+    rot[0, 1] =np.multiply(np.multiply(np.cos(a),np.sin(b)),np.sin(c))-np.multiply(np.sin(a),np.cos(c))
+    rot[0, 2] =np.multiply(np.multiply(np.cos(a),np.sin(b)),np.cos(c))+np.multiply(np.sin(a),np.sin(c))
+    rot[1, 0] =np.multiply(np.sin(a),np.cos(b))
+    rot[1, 1] =np.multiply(np.multiply(np.sin(a),np.sin(b)),np.sin(c))+np.multiply(np.cos(a),np.cos(c))
+    rot[1, 2] =np.multiply(np.multiply(np.sin(a),np.sin(b)),np.cos(c))-np.multiply(np.cos(a),np.sin(c))
+    rot[2, 0] =-np.sin(b)
+    rot[2, 1] =np.multiply(np.cos(b),np.sin(c))
+    rot[2, 2] =np.multiply(np.cos(b),np.cos(c))
+    return rot
 
+def vecNormorlize(x):
+    return x/np.linalg.norm(x)
 
-    return rots,ts_gt
-
-def Aprocess(A_rawData):
+def quat2matrix(q):
+    rot=np.zeros(3,3,len(q))
+    q=vecNormorlize(q)
+    rot[0,0]=1-2*(q(2)**2 + q(3)**2)
+    rot[0, 1] =2*(np.multiply(q(1),q(2)) + np.multiply(q(0),q(3)))
+    rot[0, 2] =2*(np.multiply(q(1),q(3)) - np.multiply(q(0),q(2)))
+    rot[1, 0] =2*(np.multiply(q(1),q(2)) - np.multiply(q(0),q(3)))
+    rot[1, 1] =1-2*(q(1)**2 + q(3)**2)
+    rot[1, 2] =2*(np.multiply(q(2),q(3)) + np.multiply(q(0),q(1)))
+    rot[2, 0] =2*(np.multiply(q(1),q(3)) + np.multiply(q(0),q(2)))
+    rot[2, 1] =2*(np.multiply(q(2),q(3)) - np.multiply(q(0),q(1)))
+    rot[2, 2] =1-2*(q(0)**2 + q(1)**2)
+    return rot
--- try.py
+++ try.py
@@ -1,13 +1,150 @@
 from scipy import io
 import numpy as np
-from helper import impData
+from helper import vec2quat,quatMulti,acc2rp,rpy2rot,vecNormorlize,quat2matrix
+
 
 
 #
 # data=io.loadmat('./vicon/viconRot1.mat')
 # rots, time= data['rots'], data['ts']
 # rots*=scale
-A,W=impData()
+def caldQ(w,t_i):
+
+    dAngle = np.linalg.norm(w) * t_i
+    dAxis = w / np.linalg.norm(w)
+    return np.append(np.cos(dAngle / 2), np.multiply(dAxis, np.sin(dAngle / 2)))
+
+
+def processA(A):
+    return rpy2rot(acc2rp(A)) # return 3*3*n
+
+def processW(W,ts):
+    _,n_data=W.shape
+    rots_W=np.zeros(3,3,n_data)
+    ti= np.append(0, ts[1:] - ts[:-1])
+    dq=caldQ(W,ti)
+    q0=np.array([1,0,0,0])
+    for i in range(n_data):
+        rots_W[:,:,i]=quat2matrix(vecNormorlize(quatMulti(q0,dq[i])))
+
+    return rots_W
+
+
+
+
+
+
+
+
+def impData():
+    sensitivity_A = 0.33  # unit:mv/g
+    scale_A = 3.3 / 1023 / sensitivity_A
+    Az_addbias =1/scale_A
+
+    sensitivity_W = 3.33   # unit:mv/deg/s
+    scale_W = 3300 / 1023 / (sensitivity_W*180/np.pi)
+
+
+    data = io.loadmat('./imu/imuRaw1.mat')
+    raw_vals, ts = data['vals'], data['ts']
+    A,W=raw_vals[:3,:],raw_vals[3:,:]
+    A_bias=np.mean(A[:,:100],axis=1)
+    A_bias[-1]=A_bias+Az_addbias
+    A=(A-A_bias)*scale_A
+    A[0:1]*=-1
+
+
+
+
+
+
+    W_bias = np.mean(W[:, :100], axis=1)
+    W = (W - W_bias) * scale_W
+    W[[0,1,2]]=W[[1,2,0]]
+
+    return A,W,ts
+
+
+def gtData():
+
+
+    data=io.loadmat('./vicon/viconRot1.mat')
+    rots, ts_gt= data['rots'], data['ts']
+
+
+    return rots,ts_gt
+
+
+
+
+
+def ukf(rots_A,rots_W,ts):
+    #init pare
+    P = np.eye(6)#State Cov
+    Q=np.eye(6)
+    Q[:3, :3]+=np.ones(3)
+    Q[3:, 3:] += np.ones(3)
+    Q*=5e-8
+
+    X=np.zeros([12,7])
+
+
+
+
+    n=6
+    x=np.array([1,0,0,0,0,0,0])
+    g = np.array([0, 0, 0, 1])
+
+    t_interval=np.append(0,ts[1:] - ts[:-1])
+
+    _, n_measure=rots.shape
+
+    for i in range(n_measure):
+
+        #calulate dq
+        dq=caldQ(x[4:],t_interval[i])
+        # dq[np.isnan(dq)]=0
+
+        #extract sigma points
+        S=np.linalg.cholesky(P+Q) #S=6*6
+        W=np.sqrt(2*n)*S
+        W=np.append(W,-W,axis=1)#W=6*12
+            #to Quater
+        X[:,:4]=quatMulti(x,vec2quat(W))
+        X[:,4:]=W[:,3:]
+        X=X.T
+
+        #Tansformation of sigma pts X
+
+
+
+
+
+
+
+
+
+
+
+
+        P=np.eye(6)
+        S=np.linalg.cholesky(P)
+
+
+    return rots_filtered
+
+if __name__=='__main__':
+    A,W,ts_imu=impData()
+    rots_A=processA(A)
+    rots_W=processW(W,ts_imu)
+
+    rots,ts_gt=gtData()
+
+    rots_ukf=ukf(rots_A,rots_W,ts_imu)
+
+
+
+
 
 
 
